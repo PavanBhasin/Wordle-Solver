b63d3891aa14c4f4099218762742f055f6f7e5ab
Pavanpal Bhasin <pbhasin@sfu.ca>
2022-02-19 04:08:32 -0800

    Added function to sort letters based on placement in correct word

--- Solver.py
+++ Solver.py
@@ -1,9 +1,6 @@
 from WordleSolver import *
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import random
 
 #Opens browser after running code
 driver = webdriver.Chrome('/usr/bin/chromedriver')
@@ -12,14 +9,34 @@ driver.get('https://www.nytimes.com/games/wordle/index.html')
 #Actions allows the program to type and click the screen without specifying elements
 actions = ActionChains(driver)
 actions.click()
-actions.send_keys('sucks\n')
+actions.send_keys('Sings\n')
 actions.perform()
+
+correctLetters = set()
+presentLetters = set()
+absentLetters = set()
+
+for x in range(1,6):
+    str = f'return document.querySelector("body > game-app").shadowRoot.querySelector("#board > game-row:nth-child(1)").shadowRoot.querySelector("div > game-tile:nth-child({x})")' 
+    letter = driver.execute_script(str)
+    if letter.get_attribute('evaluation') == 'correct':
+        correctLetters.add(letter.get_attribute('letter'))
+    elif letter.get_attribute('evaluation') == 'present':
+        presentLetters.add(letter.get_attribute('letter')) 
+    else:
+        absentLetters.add(letter.get_attribute('letter'))
+
+print (correctLetters)
+print (presentLetters)
+print (absentLetters)
 
 #print(driver.find_element_by_css_selector('game-app::shadow, game-theme-manager'))
 #test = 'game-app::shadow game-theme-manager::shadow game-row::shadow game-tile'
 #test.get_attribute('evaluation')
 
 #Iterate through nested shadowRoots to get access to tile attributes
-str = 'return document.querySelector("body > game-app").shadowRoot.querySelector("#board > game-row:nth-child(1)").shadowRoot.querySelector("div > game-tile:nth-child(1)")'
-elem = driver.execute_script(str)
-print(elem.get_attribute('evaluation'))
+#str = 'return document.querySelector("body > game-app").shadowRoot.querySelector("#board > game-row:nth-child(1)").shadowRoot.querySelector("div > game-tile:nth-child(1)")'
+#elem = driver.execute_script(str)
+#print(elem.get_attribute('evaluation'))
+
+
