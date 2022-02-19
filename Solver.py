b9ec6628e533bc68c72452b08d7f5b446c4dc564
Pavanpal Bhasin <pbhasin@sfu.ca>
2022-02-19 03:33:19 -0800

    Fixed bug where using data-state would return 'tbd'. Using evaluation instead.

--- Solver.py
+++ Solver.py
@@ -1,7 +1,9 @@
-from Wordlist import *
+from WordleSolver import *
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import random
 
 #Opens browser after running code
 driver = webdriver.Chrome('/usr/bin/chromedriver')
@@ -10,24 +12,14 @@ driver.get('https://www.nytimes.com/games/wordle/index.html')
 #Actions allows the program to type and click the screen without specifying elements
 actions = ActionChains(driver)
 actions.click()
-actions.send_keys('Guess\n')
+actions.send_keys('sucks\n')
 actions.perform()
 
-#driver.close()
-
-#value = tile.get_attribute("evaluation")
-#print(value)
-#inner_texts = [my_elem.get_attribute("outerHTML") for my_elem in driver.execute_script("""return document.querySelector('game-app').shadowRoot.querySelector('game-row').shadowRoot.querySelectorAll('game-tile[letter]')""")]
-#for inner_text in inner_texts:
- #   print(inner_text)
-
-#test = [my_elem.get_attribute("outerHTML") for my_elem in driver.execute_script("""return document.querySelector('game-app').shadowRoot.querySelector('game-row').shadowRoot.querySelector('game-tile[letter]')""")]
-#print(test)
-
-#Wait for guess to register or element cannot be located
-driver.implicitly_wait(10)
+#print(driver.find_element_by_css_selector('game-app::shadow, game-theme-manager'))
+#test = 'game-app::shadow game-theme-manager::shadow game-row::shadow game-tile'
+#test.get_attribute('evaluation')
 
 #Iterate through nested shadowRoots to get access to tile attributes
-str = 'return document.querySelector("body > game-app").shadowRoot.querySelector("#board > game-row:nth-child(1)").shadowRoot.querySelector("div > game-tile:nth-child(1)").shadowRoot.querySelector("div")'
+str = 'return document.querySelector("body > game-app").shadowRoot.querySelector("#board > game-row:nth-child(1)").shadowRoot.querySelector("div > game-tile:nth-child(1)")'
 elem = driver.execute_script(str)
-print(elem.get_attribute("data-state"))
+print(elem.get_attribute('evaluation'))
