edc2017ffb4bfd7e724ae3384f78ed78ab050969
Pavanpal Bhasin <pbhasin@sfu.ca>
2022-02-21 14:07:35 -0800

    Bug fixes

--- Solver.py
+++ Solver.py
@@ -1,10 +1,23 @@
+import random
 from Wordlist import *
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 import re
 
 def letterStatus(i):
- 
+    
+    print(wordList)
+    wordGuess = random.choice(wordList)
+
+    wordList.remove(wordGuess)
+
+    #Actions allows the program to type and click the screen without specifying elements
+    actions = ActionChains(driver)
+    actions.click()
+    actions.pause(4)
+    actions.send_keys(wordGuess + '\n')
+    actions.perform()
+
     absentLetters = ['.'] * 5
     presentLetters = ['.'] * 5
     correctLetters = ['.'] * 5
@@ -22,32 +35,17 @@ def letterStatus(i):
 
     guess = correctLetters[0] + correctLetters[1] + correctLetters[2] + correctLetters[3] + correctLetters[4]
     
-    switch = presentLetters[0] + presentLetters[1] + presentLetters[2] + presentLetters[3] + presentLetters[4]
+    modifyList(guess, absentLetters, presentLetters, correctLetters)
 
-    modifyList(guess, absentLetters, presentLetters)
-
-def modifyList(guess, absentLetters, presentLetters):
+def modifyList(guess, absentLetters, presentLetters, correctLetters):
     global wordList
-    for word in wordList[:]:
-        if word[0] == absentLetters[0]:
-            wordList.remove(word)
-        elif word[1] == absentLetters[1]:
-            wordList.remove(word)
-        elif word[2] == absentLetters[2]:
-            wordList.remove(word)
-        elif word[3] == absentLetters[3]:
-            wordList.remove(word)
-        elif word[4] == absentLetters[4]:
-            wordList.remove(word)
-    print(wordList)
-
+    
+    wordList = [word for word in wordList if all(letter not in word for letter in absentLetters if letter not in correctLetters if letter not in presentLetters)]
+  
     regex = re.compile(guess)
     wordList = [word for word in wordList if re.match(regex, word)]
-    print(wordList)
 
     wordList = [word for word in wordList if all(letter in word for letter in presentLetters if letter != '.')]
-    
-    print(wordList)
 
     for word in wordList[:]:
         if word[0] == presentLetters[0]:
@@ -60,20 +58,10 @@ def modifyList(guess, absentLetters, presentLetters):
             wordList.remove(word)
         elif word[4] == presentLetters[4]:
             wordList.remove(word)
-    print(wordList)
-
+            
 #Opens browser after running code
 driver = webdriver.Chrome('/usr/bin/chromedriver')
 driver.get('https://www.nytimes.com/games/wordle/index.html')
 
-#Actions allows the program to type and click the screen without specifying elements
-actions = ActionChains(driver)
-actions.click()
-actions.send_keys('rates\n')
-actions.perform()
-
-correctWords = set()
-presentWords = set()
-absentWords = set()
-
-letterStatus(1)
+for x in range(1,7):
+    letterStatus(x)
