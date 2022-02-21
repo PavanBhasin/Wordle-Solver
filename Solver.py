31a5c54772516863fbe275fba153902856010e50
Pavanpal Bhasin <pbhasin@sfu.ca>
2022-02-20 23:46:46 -0800

    Refactored code to create letter status function and added functionality to filter for words with letters in the correct spots

--- Solver.py
+++ Solver.py
@@ -1,6 +1,36 @@
-from WordleSolver import *
+from Wordlist import *
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
+import re
+
+first = second = third = fourth = fifth = "."
+
+def letterStatus(i):
+    global first, second, third, fourth, fifth
+    for x in range(1,6):
+        str = f'return document.querySelector("body > game-app").shadowRoot.querySelector("#board > game-row:nth-child({i})").shadowRoot.querySelector("div > game-tile:nth-child({x})")' 
+        tile = driver.execute_script(str)
+        if tile.get_attribute('evaluation') == 'correct':
+            correctLetters.add(tile.get_attribute('letter'))
+            if x == 1:
+                first = tile.get_attribute('letter')
+            elif x == 2:
+                second = tile.get_attribute('letter')
+            elif x == 3: 
+                third = tile.get_attribute('letter')
+            elif x == 4:
+                fourth = tile.get_attribute('letter')
+            elif x == 5:
+                fifth = tile.get_attribute('letter')
+
+        elif tile.get_attribute('evaluation') == 'present':
+            presentLetters.add(tile.get_attribute('letter')) 
+        else:
+            absentLetters.add(tile.get_attribute('letter'))
+
+    guess = first + second + third + fourth + fifth
+
+    return guess
 
 #Opens browser after running code
 driver = webdriver.Chrome('/usr/bin/chromedriver')
@@ -9,34 +39,20 @@ driver.get('https://www.nytimes.com/games/wordle/index.html')
 #Actions allows the program to type and click the screen without specifying elements
 actions = ActionChains(driver)
 actions.click()
-actions.send_keys('Sings\n')
+actions.send_keys('tacho\n')
 actions.perform()
 
 correctLetters = set()
 presentLetters = set()
 absentLetters = set()
 
-for x in range(1,6):
-    str = f'return document.querySelector("body > game-app").shadowRoot.querySelector("#board > game-row:nth-child(1)").shadowRoot.querySelector("div > game-tile:nth-child({x})")' 
-    letter = driver.execute_script(str)
-    if letter.get_attribute('evaluation') == 'correct':
-        correctLetters.add(letter.get_attribute('letter'))
-    elif letter.get_attribute('evaluation') == 'present':
-        presentLetters.add(letter.get_attribute('letter')) 
-    else:
-        absentLetters.add(letter.get_attribute('letter'))
+correctWords = set()
+presentWords = set()
+absentWords = set()
 
-print (correctLetters)
-print (presentLetters)
-print (absentLetters)
+guess = letterStatus(1)
+wordList = [word for word in wordList if all(letter not in word for letter in absentLetters)]
 
-#print(driver.find_element_by_css_selector('game-app::shadow, game-theme-manager'))
-#test = 'game-app::shadow game-theme-manager::shadow game-row::shadow game-tile'
-#test.get_attribute('evaluation')
-
-#Iterate through nested shadowRoots to get access to tile attributes
-#str = 'return document.querySelector("body > game-app").shadowRoot.querySelector("#board > game-row:nth-child(1)").shadowRoot.querySelector("div > game-tile:nth-child(1)")'
-#elem = driver.execute_script(str)
-#print(elem.get_attribute('evaluation'))
-
-
+regex = re.compile(guess)
+wordList = [word for word in wordList if re.match(regex, word)]
+print(wordList)
